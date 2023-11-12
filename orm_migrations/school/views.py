0c7c081200ab1436aacaf2ca743f05da569cc351
Nikiforov vyacheslav <slava-2585@yandex.ru>
2023-11-12 18:49:23 +0300

    migrate

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    object_list = Student.objects.all().order_by('name')
+    context = {
+        'object_list': object_list
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
